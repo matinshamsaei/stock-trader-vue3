bb24c3bcf18838568395d35ab8269e86600da8ba
matin <matin3061379@gmail.com>
2022-06-13 16:07:06 +0430

    Fix Main And App

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,62 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView } from "vue-router";
+import AppHeader from "./components/Header";
+import { stocks } from "./stocks";
+
+const stocksStore = stocks();
+stocksStore.initStock();
 </script>
 
 <template>
-  <header>
-    
-  </header>
+  <div id="app">
+    <div class="container py-5">
+      <app-header></app-header>
 
-  <RouterView />
+      <div class="row">
+        <div class="px-4 pt-3 pb-5">
+          <transition name="slide" mode="out-in">
+            <router-view></router-view>
+          </transition>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
+.px-4.pt-3.pb-5 {
+  width: 100%;
+}
 
+.slide-enter-active {
+  animation: slide-in 300ms ease-out forwards;
+}
+
+.slider-leave-active {
+  animation: slide-out 300ms ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import VueResource from "vue-resource";
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
+
+app.use(VueResource);
+app.http.options.root = "https://stock-trade-eb949.firebaseio.com/";
 
 app.use(createPinia());
 app.use(router);
